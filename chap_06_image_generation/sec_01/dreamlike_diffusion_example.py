8ec2b59f111c1b2c31999bdcb41447db1e2d1347
SungYong Lee <saintdragon2@gmail.com>
2023-11-05 17:53:01 +0900

    6장까지

---
+++ chap_06_image_generation/sec_01/dreamlike_diffusion_example.py
@@ -0,0 +1,19 @@
+# 필요한 라이브러리 임포트하기
+from diffusers import StableDiffusionPipeline
+import torch
+
+# 사용할 이미지 생성 모델의 ID 설정하기
+model_id="dreamlike-art/dreamlike-diffusion-1.0"
+# 사전 훈련된 모델을 로드해 파이프라인 생성하기
+pipe=StableDiffusionPipeline.from_pretrained(model_id)
+
+# 모델을 GPU로 이동하기
+pipe=pipe.to("cuda")
+
+prompt="dreamlikeart, a grungy woman with rainbow hair, travelling between dimensions, dynamic pose, happy, soft eyes and narrow chin, extreme bokeh, dainty figure, long hair straight down, torn kawaii shirt and baggy jeans, In style of by Jordan Grimmer and greg rutkowski, crisp lines and color, complex background, particles, lines, wind, concept art, sharp focus, vivid colors"
+
+# 프롬프트를 기반으로 이미지 생성하기
+image=pipe(prompt).images[0]
+
+# 생성한 이미지를 result.jpg 파일로 저장하기
+image.save("./result.jpg")
---
+++ chap_06_image_generation/sec_03/generate_image.py
@@ -0,0 +1,37 @@
+from dalle2_image_gen import generate_dalle_image
+from dreamlike_diffusion_image_gen import generate_dreamlike_image
+import pandas as pd
+import torch
+import time
+
+
+def generate_images_for_songs(csv_file):
+    df_playlist=pd.read_csv(csv_file, sep=';')
+   
+    is_cuda_or_mps_available=torch.cuda.is_available() or torch.backends.mps.is_available()
+   
+    image_file_path=list()
+   
+    response_str='다음 곡의 이미지를 생성했습니다.'
+   
+    for i, row in df_playlist.iterrows():
+        if row['mp3'] == 'Not found':
+            image_file_path.append(None)
+        else:
+            if is_cuda_or_mps_available:
+                image_file=generate_dreamlike_image(row['Title'], row['Artist'])
+            else:
+                try:
+                    image_file=generate_dalle_image(row['Title'], row['Artist'])
+                except:
+                    print('Something went to wrong...')
+                    image_file=None
+                time.sleep(5)
+            image_file_path.append(image_file)
+ 
+            response_str+=f'\n{row["Title"]}\t{row["Artist"]}\t{image_file}'
+ 
+    df_playlist['image_file']=image_file_path
+    df_playlist.to_csv(csv_file, sep=';', index=False)
+    
+    return response_str
