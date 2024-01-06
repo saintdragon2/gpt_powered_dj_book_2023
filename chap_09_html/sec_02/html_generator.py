1ad970e83e653869e7b68e894c60f115ca858aee
SungYong Lee <saintdragon2@gmail.com>
2024-01-06 13:42:04 +0900

    완결!

--- chap_09_html/sec_02/html_generator.py
+++ chap_09_html/sec_02/html_generator.py
@@ -1,19 +1,23 @@
 import os
 import pandas as pd
 
-def create_html(csv_file):
+def create_html(csv_file, youtube_title, youtube_description):
     df = pd.read_csv(csv_file, sep=';')
 
-    html="""
+    html=f"""
     <!DOCTYPE html>
     <html>
     <head>
-        <title>Music Collection</title>
+        <title>{youtube_title}</title>
         <link rel="stylesheet" href="https://stackpath.bootstrapcdn.com/bootstrap/4.3.1/css/bootstrap.min.css">
     </head>
 
     <body>
     <div class="container">
+    <h1>{youtube_title}</h1>
+    <hr/>
+    <p>{youtube_description}</p>
+    <hr/>
     """
 
     for i in range(0, len(df), 2):
@@ -47,7 +51,11 @@ def create_html(csv_file):
     with open(html_file, 'w', encoding='UTF-8') as f:
         f.write(html)
 
-    return html_file
+    return f'\n{html_file}에 HTML 파일을 저장했습니다.'
 
 if __name__ == '__main__':
-    create_html('./playlist/eric_clapton.csv')
+    create_html(
+        './playlist/eric_clapton.csv',
+        'Eric Clapton Best',
+        'Eric Clapton의 명곡들을 추천해드립니다. Layla, Tears in Heaven, Wonderful Tonight 등 Eric Clapton의 기타와 노래를 감상해보세요.'
+    )
